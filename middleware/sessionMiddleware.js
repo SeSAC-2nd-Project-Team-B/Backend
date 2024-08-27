a54d58a942d5fcea6c7d46615d4d0bc367e76ecc
ymind14563 <ymind14563@gmail.com>
2024-08-27 23:56:49 +0900

    feat: session

---
+++ middleware/sessionMiddleware.js
@@ -0,0 +1,10 @@
+const session = require("express-session");
+
+const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+});
+
+module.exports = sessionMiddleware;
