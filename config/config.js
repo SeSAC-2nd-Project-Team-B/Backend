56132022a4cb4f1a04dd02abefa38e67a4077f93
ymind14563 <ymind14563@gmail.com>
2024-08-27 23:56:42 +0900

    fix: env path

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-const envFile = process.env.NODE_ENV;
-
-dotenv.config({
-  path: path.resolve(__dirname, envFile),
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+dotenv.config({ 
+  path: path.resolve(__dirname, '..', envFile)
 });
+
 
 const config = {
   "development": {
