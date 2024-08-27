aaffe4be0fcd9f3507f586bb0e3082d6fa96e431
yjyoo6831 <cenabro@naver.com>
2024-08-28 00:33:17 +0900

    feat: model

---
+++ models/ReportModel.js
@@ -0,0 +1,42 @@
+// 신고 모델 정의
+const ReportModel = (sequelize, DataTypes) =>{
+    const Report = sequelize.define(
+        'Report', 
+        { 
+        // 신고 수 인덱스
+        reportId:{
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        // 상품 인덱스 
+        productId:{
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        // 유저 인덱스
+        userId:{
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        reportCount:{
+            type:DataTypes.BIGINT,
+            allowNull:false,
+            defaultValue:0
+        }
+    },
+    {
+        freezeTableName : true, 
+        timestamps : true, 
+    }
+);
+    return Report;
+}
+
+module.exports=ReportModel;
+
